84ae0450b21d98f8e183f2d1046d6c751cc7c73d
MyFirstGitEver <fordevok@gmail.com>
2023-08-14 14:28:27 +0700

    Initial Commit

---
+++ gigamall-app/gigamall_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  gigamall_appApp.swift
+//  gigamall-app
+//
+//  Created by FVFH4069Q6L7 on 14/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct gigamall_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gigamall-appUITests/gigamall_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  gigamall_appUITestsLaunchTests.swift
+//  gigamall-appUITests
+//
+//  Created by FVFH4069Q6L7 on 14/08/2023.
+//
+
+import XCTest
+
+final class gigamall_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
